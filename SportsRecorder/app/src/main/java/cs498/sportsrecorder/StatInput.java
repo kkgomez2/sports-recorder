aca06403d46aa95329cf1dade88feb50c46e087a
Crook <racrook2@illinois.edu>
2016-03-29 16:16:54 -0500

    added summary page with fragments

---
+++ SportsRecorder/app/src/main/java/cs498/sportsrecorder/StatInput.java
@@ -0,0 +1,13 @@
+package cs498.sportsrecorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat_input);
+    }
+}
