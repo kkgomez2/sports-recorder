390248070e16d2f72a0bc73b8e27bab8e44f46a9
Crook <racrook2@illinois.edu>
2016-03-31 18:07:02 -0500

    can now see summary during game

--- SportsRecorder/app/src/main/java/cs498/sportsrecorder/StatInput.java
+++ SportsRecorder/app/src/main/java/cs498/sportsrecorder/StatInput.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileOutputStream;
@@ -16,83 +17,98 @@ import java.util.Locale;
 
 public class StatInput extends AppCompatActivity {
     private GameData gameData;
+    private String filename;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stat_input);
         gameData = new GameData();
-
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy hh:mm", Locale.US);
+        filename = "Game on " + sdf.format(new Date());
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
 
-        // Checks the orientation of the screen
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_summary);
+            TextView textview = (TextView)findViewById(R.id.summary_text);
+            textview.setText(gameData.getSummary());
+
+        } else {
+            setContentView(R.layout.activity_stat_input);
         }
     }
 
     public void addMadeFreeThrow(View view){
         gameData.addMadeFreeThrow();
         Toast.makeText(this, "Made free throw recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 
     public void addMissedFreeThrow(View view){
         gameData.addMissedFreeThrow();
         Toast.makeText(this, "Missed free throw recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 
     public void addMadeTwoPoint(View view){
         gameData.addMadeTwoPoint();
         Toast.makeText(this, "Made field goal recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 
     public void addMissedTwoPoint(View view){
         gameData.addMissedTwoPoint();
         Toast.makeText(this, "Missed field goal recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 
     public void addMadeThreePoint(View view){
         gameData.addMadeThreePoint();
         Toast.makeText(this, "Made three pointer recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 
     public void addMissedThreePoint(View view){
         gameData.addMissedThreePoint();
         Toast.makeText(this, "Missed three pointer recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 
     public void addRebound(View view){
         gameData.addRebound();
         Toast.makeText(this, "Rebound recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 
     public void addAssist(View view) {
         gameData.addAssist();
         Toast.makeText(this, "Assist recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 
     public void addSteal(View view){
         gameData.addSteal();
         Toast.makeText(this, "Steal recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 
     public void addBlock(View view){
         gameData.addBlock();
         Toast.makeText(this, "Block recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 
     public void endQuarter(View view){
         gameData.endQuarter();
         Toast.makeText(this, "End of quarter recorded", Toast.LENGTH_SHORT).show();
+        saveToFile();
         if(gameData.getQuarter() > 3){
             // Game is over (Past 4th quarter, zero based), save the game.
-            SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy hh:mm", Locale.US);
+            /*SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy hh:mm", Locale.US);
             String filename = "Game on " + sdf.format(new Date());
             Log.d("Sports", "Saving to " + filename);
 
@@ -103,7 +119,7 @@ public class StatInput extends AppCompatActivity {
                 out.close();
             } catch (Exception e){
                 e.printStackTrace();
-            }
+            }*/
 
             // Launch the Summary activity.
             Intent intent = new Intent(this, Summary.class);
@@ -112,8 +128,22 @@ public class StatInput extends AppCompatActivity {
         }
     }
 
+    private void saveToFile() {
+        Log.d("Sports", "Saving to " + filename);
+
+        try {
+            FileOutputStream out = openFileOutput(filename, Context.MODE_PRIVATE);
+            String saveContent = gameData.getSaveContent();
+            out.write(saveContent.getBytes());
+            out.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     public void undo(View view){
         gameData.undo();
         Toast.makeText(this, "Undid last action", Toast.LENGTH_SHORT).show();
+        saveToFile();
     }
 }
